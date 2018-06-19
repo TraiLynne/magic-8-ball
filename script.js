f7827d964ddb4e391b44236ee1db5ab1f562926c
Trai Lynne <trailynne@outlook.com>
2018-06-18 20:14:12 -0600

    JS: Defined magic 8 ball object and it's property

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
+    //Magic 8 Ball Object & Properties
+    var magic8Ball = {};
 
+    magic8Ball.listOfAnswers = [
+        "Yes",
+        "Fasho",
+        "Sure, why not?",
+        "Speak it into Existence",
+        "Make it Happen, Cap'n",
+        "You already know!",
+        "No",
+        "Probably Not",
+        "Get your entire life",
+        "Bye Felicia",
+        "You could do something more productive",
+        "It's too soon to tell",
+        "Just wait until you see",
+        "Maybe",
+        "I Don't Even Know",
+        "Are you sure you want to know?",
+        "Is it really what YOU want?",
+        "That's not a good look"
+    ]
 })
